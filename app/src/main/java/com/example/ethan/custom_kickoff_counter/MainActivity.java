0e02dcb5ac0317991a738653357885a880244e0b
Ethan Hammond <ethanhammond12099@gmail.com>
2016-11-15 23:02:41 -0500

    Swapped from using Date util to Calendar util

--- app/src/main/java/com/example/ethan/custom_kickoff_counter/MainActivity.java
+++ app/src/main/java/com/example/ethan/custom_kickoff_counter/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ethan.custom_kickoff_counter;
 
+import android.icu.text.SimpleDateFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public static int timeUntilKickoff(Calendar currentDate, Calendar kickoffDate) {
 
+    }
+
+    public Calendar setKickoffDate() {
+
+    }
+
+    public Calendar getCurrentDate() {
+        Calendar currentDate = new Calendar.getInstance();
+        return currentDate;
+    }
 }
